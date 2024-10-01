c6c872354c452d78ebd1fecfbbd423a3a2f75597
Bulgogi-Pizza <quitendexit@gmail.com>
2024-10-01 18:35:32 +0900

    Revert "feat : 질문, 질문상세 페이지 컴포넌트 완성"
    This reverts commit 7f8f9ed47785a85ce5f8d0fff1441d12da9344d9.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Router from "./shared/Router";
-import React, { useEffect, useState, } from "react";
-import QuestionList from "./pages/QuestionList";
-import QuestionDetail from "./pages/QuestionDetail";
-import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<QuestionList />} />
-        <Route path="/question/:match" element={<QuestionDetail />} />
-      </Routes>
-    </div>
+    <>
+      <Router />
+    </>
   );
 }
 
