a9de9b6eab5cc8b61bf734400ee2668b7f548cd9
tkddn0321 <godsknight58@naver.com>
2024-10-21 16:50:09 +0900

    fetch : DataGrid 변환

--- src/pages/manager/CourseManager.js
+++ src/pages/manager/CourseManager.js
@@ -3,6 +3,7 @@ import { Button, Modal, Typography, TextField, Box, FormControl, InputLabel, Sel
 import moment from 'moment';
 import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { DataGrid } from '@mui/x-data-grid'; // DataGrid 임포트
 import apiClient from '../../shared/apiClient';
 
 const CourseManager = () => {
@@ -12,6 +13,7 @@ const CourseManager = () => {
     const [selectedCourses, setSelectedCourses] = useState([]); // 선택한 강의 상태 관리
 
     // 폼 입력 상태 관리
+    const [newEventId, setNewEventId] = useState(''); // ID 추가
     const [newEventClassroom, setNewEventClassroom] = useState('');
     const [newEventTitle, setNewEventTitle] = useState('');
     const [newEventDescription, setNewEventDescription] = useState('');
@@ -106,6 +108,7 @@ const CourseManager = () => {
                 fetchEvents(); // 수정 후 이벤트 목록 새로 고침
             })
             .catch(error => {
+                console.log(updatedCourse);
                 console.error('강의 수정에 실패했습니다.', error);
             });
     };
@@ -113,6 +116,7 @@ const CourseManager = () => {
     // 이벤트 추가 또는 업데이트 핸들러
     const handleSaveEvent = () => {
         const newCourse = {
+            id: newEventId,
             classroomName: newEventClassroom,
             title: newEventTitle,
             description: newEventDescription,
@@ -175,66 +179,144 @@ const CourseManager = () => {
         const courseToEdit = events.find(event => event.id === selectedCourses[0]);
         if (courseToEdit) {
             setSelectedEvent(courseToEdit);
-            setNewEventClassroom(courseToEdit.classroomName);
-            setNewEventTitle(courseToEdit.courseTitle);
-            setNewEventDescription(courseToEdit.description);
-            setNewEventStart(courseToEdit.startDate);
-            setNewEventEnd(courseToEdit.EndDate);
+            setNewEventId(courseToEdit.id);
+            setNewEventClassroom(courseToEdit.classroomName || '');
+            setNewEventTitle(courseToEdit.title || '');
+            setNewEventDescription(courseToEdit.description || '');
+            setNewEventStart(courseToEdit.startDate ? moment(courseToEdit.startDate) : moment());
+            setNewEventEnd(courseToEdit.endDate ? moment(courseToEdit.endDate) : moment());
             setNewEventLayoutImageUrl(courseToEdit.layoutImageUrl);
             setEditMode(true);
             setOpen(true);
         }
     };
 
-    // 체크박스 선택/해제 핸들러
-    const handleSelectCourse = (courseId) => {
-        setSelectedCourses((prevSelected) =>
-            prevSelected.includes(courseId)
-                ? prevSelected.filter(id => id !== courseId) // 선택 해제
-                : [...prevSelected, courseId] // 선택 추가
-        );
-    };
-
     return (
         <LocalizationProvider dateAdapter={AdapterMoment}>
             <div>
                 {/* 강의 리스트 테이블 */}
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>선택</TableCell>
-                            <TableCell>번호</TableCell>
-                            <TableCell>강사명</TableCell>
-                            <TableCell>강의실명</TableCell>
-                            <TableCell>강의명</TableCell>
-                            <TableCell>설명</TableCell>
-                            <TableCell>시작 날짜</TableCell>
-                            <TableCell>종료 날짜</TableCell>
-                            <TableCell>좌석 배치도</TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {events.map((event, index) => (
-                            <TableRow key={index}>
-                                <TableCell sx={{ paddingLeft: '8px' }}>
-                                    <Checkbox
-                                        checked={selectedCourses.includes(event.id)}
-                                        onChange={() => handleSelectCourse(event.id)}
-                                        sx={{ m1: -10 }}
-                                    />
-                                </TableCell>
-                                <TableCell>{event.id}</TableCell>
-                                <TableCell>{event.instructor}</TableCell>
-                                <TableCell>{event.classroomName}</TableCell>
-                                <TableCell>{event.title}</TableCell>
-                                <TableCell>{event.description}</TableCell>
-                                <TableCell>{event.startDate}</TableCell>
-                                <TableCell>{event.endDate}</TableCell>
-                                <TableCell>{event.layoutImageUrl}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                <Box sx={{ height: 650, width: '100%' }}>
+                    <DataGrid
+                        sx={{
+                            backgroundColor: "white", // 배경색
+                            border: "none", // 테두리
+                            "--DataGrid-rowBorderColor": "transparent",
+                            "& .MuiDataGrid-cell": {
+                                border: "none",
+                            },
+                            "& .MuiDataGrid-row": {
+                                borderBottom: "1px solid #f6f8fa",
+                            },
+                            "& .MuiDataGrid-filler": {
+                                display: "none",
+                            },
+                            "& .MuiDataGrid-footerContainer": {
+                                border: "none",
+                            },
+                            "& .MuiDataGrid-columnHeaderTitleContainer": {
+                                fontSize: "12px",
+                                fontWeight: "bold",
+                                color: "#222831",
+                            },
+                            "& .MuiDataGrid-columnSeparator--sideRight": {
+                                display: "none",
+                            },
+                            "& .MuiDataGrid-columnHeader": {
+                                color: "black", // 헤더 글자색
+                                border: "none",
+                            },
+                        }}
+                        localeText={{
+                            // 선택된 행 수 텍스트 변경
+                            footerRowSelected: (count) => `${count}개 선택됨`, // 예: "11개 선택됨"
+                            // 필터 관련 텍스트 변경
+                            filterOperatorContains: "포함",
+                            filterOperatorEquals: "같음",
+                            filterOperatorStartsWith: "시작함",
+                            filterOperatorEndsWith: "끝남",
+                            filterOperatorIs: "이것",
+                            filterOperatorDoesNotContain: "포함하지 않음",
+                            filterOperatorDoesNotEqual: "같지 않음",
+                            filterOperatorIsAnyOf: "다음 중 하나",
+                            filterOperatorNot: "아니오",
+                            filterOperatorAfter: "이후",
+                            filterOperatorBefore: "이전",
+                            filterOperatorIsEmpty: "비어 있음",
+                            filterOperatorIsNotEmpty: "비어 있지 않음",
+
+                            // 필터 메뉴 텍스트
+                            filterPanelInputLabel: "필터",
+                            filterPanelAddFilter: "필터 추가",
+                            filterPanelDeleteIconLabel: "삭제",
+                            filterPanelOperator: "연산자",
+                            filterPanelColumns: "데이터 열",
+                            filterPanelValue: "값",
+                            filterPanelOperatorAnd: "그리고",
+                            filterPanelOperatorOr: "또는",
+                            sortAscending: "오름차순 정렬",
+                            sortDescending: "내림차순 정렬",
+                            columnMenuSortAsc: "오름차순 정렬", // "Sort Ascending" 텍스트 변경
+                            columnMenuSortDesc: "내림차순 정렬", // "Sort Descending" 텍스트 변경
+                            columnMenuShowAll: "모두 표시", // "Show All" 텍스트 변경
+                            columnMenuFilter: "필터", // "Filter" 텍스트 변경
+                            columnMenuUnsort: "정렬 해제", // "Unsort" 텍스트 변경
+                            columnMenuHideColumn: "숨기기", // "Hide" 텍스트 변경
+                            columnMenuHideOn: "숨기기", // "Hide on" 텍스트 변경
+                            columnMenuManageColumns: "관리", // "Manage" 텍스트 변경
+                            // 페이지 관련 텍스트 변경
+                            page: "페이지",
+                            noRowsLabel: "데이터가 없습니다.",
+                            noResultsOverlayLabel: "결과가 없습니다.",
+                            errorOverlayDefaultLabel: "오류가 발생했습니다.",
+                            // 페이지네이션 관련 텍스트
+                            pageSize: "페이지 크기",
+                            pageSizeOptions: ["5", "10", "20"],
+                        }}
+                        rows={events} // 데이터
+
+
+                        columns={[
+
+                            { field: 'id', headerName: '번호', flex: 0.5 },
+                            { field: 'instructor', headerName: '강사명', flex: 1 },
+                            { field: 'classroomName', headerName: '강의실명', flex: 1 },
+                            { field: 'title', headerName: '강의명', flex: 1.3 },
+                            { field: 'description', headerName: '설명', flex: 1.3 },
+                            { field: 'startDate', headerName: '시작 날짜', flex: 1.5, ml: 30 },
+                            { field: 'endDate', headerName: '종료 날짜', flex: 1.5, ml: 30 },
+                            {
+                                field: 'layoutImageUrl',
+                                headerName: '좌석 배치도',
+                                flex: 1.5,
+                                renderCell: (params) => {
+                                    const classroomName = params.row.classroomName;
+                                    const layoutUrl = params.value;
+
+                                    return layoutUrl ? (
+                                        <a href={layoutUrl} target="_blank" rel="noopener noreferrer">
+                                            {`${classroomName} 배치도`}
+                                        </a>
+                                    ) : '';
+                                },
+                            },
+                        ]}
+                        initialState={{
+                            pagination: {
+                                paginationModel: {
+                                    pageSize: 10,
+                                },
+                            },
+                        }}
+                        pageSizeOptions={[10]}
+                        checkboxSelection // 행 선택을 위한 체크박스 추가
+                        onRowSelectionModelChange={(newSelection) => {
+                            setSelectedCourses(newSelection); // 상태 업데이트
+
+                        }}
+
+                        selectionModel={selectedCourses} // 선택된 ID 목록
+                    />
+                </Box>
 
                 {/* 등록, 수정, 삭제 버튼 */}
                 <Box mt={2} sx={{ display: "flex", justifyContent: "flex-end" }}>
@@ -306,24 +388,12 @@ const CourseManager = () => {
                                     ))}
                                 </Select>
                             </FormControl>
-                            <Box sx={{ display: "grid" }}>
-                                <TextField
-                                    fullwidth
-                                    label="설명"
-                                    value={newEventDescription}
-                                    onChange={(e) => {
-                                        setNewEventDescription(e.target.value);
-                                    }}
-                                />
-                            </Box>
                             <DatePicker
                                 label="시작 날짜"
                                 value={newEventStart}
                                 onChange={(newValue) => setNewEventStart(moment(newValue))}
                                 renderInput={(params) => <TextField {...params} fullWidth style={{ marginBottom: '20px' }} />}
                             />
-                            <br />
-                            <br />
                             <DatePicker
                                 label="종료 날짜"
                                 value={newEventEnd}
@@ -334,11 +404,25 @@ const CourseManager = () => {
                         <Box sx={{ display: "grid" }}>
                             <TextField
                                 fullwidth
+                                label="설명"
+                                value={newEventDescription}
+                                onChange={(e) => {
+                                    setNewEventDescription(e.target.value);
+                                }}
+                                style={{ marginTop: '16px' }}
+                                multiline  // TextField를 textarea로 변경
+                                rows={4}   // 표시할 줄 수 (필요에 따라 조정 가능)
+                            />
+                        </Box>
+                        <Box sx={{ display: "grid" }}>
+                            <TextField
+                                fullwidth
                                 label="좌석 배치 url"
                                 value={newEventLayoutImageUrl}
                                 onChange={(e) => {
                                     setNewEventLayoutImageUrl(e.target.value);
                                 }}
+                                style={{ marginTop: '16px' }}
                             />
                         </Box>
 
